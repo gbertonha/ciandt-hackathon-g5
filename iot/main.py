224aa8207c1f5586f922cb2f6321b284d202b7c5
andrek-ciandt <andrek@ciandt.com>
2019-09-13 13:29:37 +0900

    Update main.py
    Update class main.py

--- iot/main.py
+++ iot/main.py
@@ -9,8 +9,8 @@ from firebase_admin import firestore
 from pytz import timezone
 
 
-class Main:
-
+class Iot(object):
+    db = None
 
     def __init__(self):
         sensor1Pin = 16
@@ -24,39 +24,41 @@ class Main:
         GPIO.setup(sensor3Pin, GPIO.IN)
 
         # Events
-        GPIO.add_event_detect(sensor1Pin, GPIO.BOTH, callback=sensor1, bouncetime=300)
-        GPIO.add_event_detect(sensor2Pin, GPIO.BOTH, callback=sensor2, bouncetime=300)
-        GPIO.add_event_detect(sensor3Pin, GPIO.BOTH, callback=sensor3, bouncetime=300)
+        GPIO.add_event_detect(sensor1Pin, GPIO.BOTH, callback=self.sensor1, bouncetime=300)
+        GPIO.add_event_detect(sensor2Pin, GPIO.BOTH, callback=self.sensor2, bouncetime=300)
+        GPIO.add_event_detect(sensor3Pin, GPIO.BOTH, callback=self.sensor3, bouncetime=300)
 
         # Use a service account
         cred = credentials.Certificate('key.json')
         firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
 
     def sensor1(self, channel):
         if GPIO.input(channel):
             print("Sensor1 Detected!")
-            sendToFirestore('sensor1', random.randint(1,100))
+            self.sendToFirestore('sensor1', random.randint(1,100))
 
     def sensor2(self, channel):
         if GPIO.input(channel):
             print("Sensor2 Detected!")
-            sendToFirestore('sensor2', random.randint(1,100))
+            self.sendToFirestore('sensor2', random.randint(1,100))
 
     def sensor3(self, channel):
         if GPIO.input(channel):
             print("Sensor3 Detected!")
-            sendToFirestore('sensor3', random.randint(1,100))
+            self.sendToFirestore('sensor3', random.randint(1,100))
 
     def sendToFirestore(self, collection, value):
-        doc_ref = db.collection(collection).get()
+        doc_ref = self.db.collection(collection).get()
 
         try:
-            for doc in doc_ref:
-                doc.set({
+            for doc in doc_ref:                
+                item = self.db.collection(collection).document(doc.id)
+                item.set({
                     u'value': value
                 }, merge=True)
-        except e:
-            print(e)
+        except Exception as e:
+            print(str(e))
 
     def run(self):
         try:
@@ -66,4 +68,6 @@ class Main:
         finally:
             GPIO.cleanup()
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
+    m = Iot()
+    m.run()
