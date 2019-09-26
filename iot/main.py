d0c55a93952679e167948b8ee14a6f07d05ec2e2
bertonha <geovanneb@gmail.com>
2019-09-26 13:28:24 +0900

    Pressure sensor setup.

--- iot/main.py
+++ iot/main.py
@@ -4,44 +4,55 @@ import RPi.GPIO as GPIO
 import datetime
 import subprocess
 import firebase_admin
+import Adafruit_DHT
 from firebase_admin import credentials
 from firebase_admin import firestore
 from pytz import timezone
-
+from vendor.Adafruit_BMP085.Adafruit_BMP085 import BMP085
 
 class Iot(object):
     db = None
 
+    sensor1Pin = 18
+    bmp = BMP085(0x77, debug=True)
+
     def __init__(self):
-        sensor1Pin = 16
-        sensor2Pin = 21
-        sensor3Pin = 20
+        print("init")
+        #sensor2Pin = 21
+        #sensor3Pin = 20
         GPIO.setmode(GPIO.BCM)
 
         # Setup
-        GPIO.setup(sensor1Pin, GPIO.IN)
-        GPIO.setup(sensor2Pin, GPIO.IN)
-        GPIO.setup(sensor3Pin, GPIO.IN)
+        #GPIO.setup(sensor1Pin, GPIO.IN)
+        #GPIO.setup(sensor2Pin, GPIO.IN)
+        #GPIO.setup(sensor3Pin, GPIO.IN)
 
         # Events
-        GPIO.add_event_detect(sensor1Pin, GPIO.BOTH, callback=self.sensor1, bouncetime=300)
-        GPIO.add_event_detect(sensor2Pin, GPIO.BOTH, callback=self.sensor2, bouncetime=300)
-        GPIO.add_event_detect(sensor3Pin, GPIO.BOTH, callback=self.sensor3, bouncetime=300)
+        #GPIO.add_event_detect(sensor1Pin, GPIO.BOTH, callback=self.sensor1, bouncetime=300)
+        
+        #GPIO.add_event_detect(sensor2Pin, GPIO.BOTH, callback=self.sensor2, bouncetime=300)
+        #GPIO.add_event_detect(sensor3Pin, GPIO.BOTH, callback=self.sensor3, bouncetime=300)
 
         # Use a service account
         cred = credentials.Certificate('key.json')
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
 
-    def sensor1(self, channel):
-        if GPIO.input(channel):
-            print("Sensor1 Detected!")
-            self.sendToFirestore('sensor1', random.randint(1,100))
-
-    def sensor2(self, channel):
-        if GPIO.input(channel):
+    def sensor1(self):
+        humidity, temperature = Adafruit_DHT.read_retry(11, self.sensor1Pin)
+        print(humidity)
+        print(temperature)
+        if temperature is not None:
+          self.sendToFirestore('sensor1', round(temperature, 2))
+        if humidity is not None:
+          self.sendToFirestore('sensor2', round(humidity, 2))
+    
+    def sensor2(self):
+        pressure = self.bmp.readPressure()
+        print(pressure)
+        if pressure is not None:
             print("Sensor2 Detected!")
-            self.sendToFirestore('sensor2', random.randint(1,100))
+            self.sendToFirestore('sensor3', int(round(pressure / 100.0, 2)))
 
     def sensor3(self, channel):
         if GPIO.input(channel):
@@ -63,7 +74,9 @@ class Iot(object):
     def run(self):
         try:
             while True:
-                time.sleep(1)
+                self.sensor1()
+                self.sensor2()
+                time.sleep(20)
 
         finally:
             GPIO.cleanup()
