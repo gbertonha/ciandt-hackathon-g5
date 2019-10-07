f379ba652af86e3210af0c8ec912c26841363213
Geovanne Borges Bertonha <gbertonha@ciandt.com>
2019-10-07 13:18:58 +0900

    JSlint

--- web-ui/public/js/script.js
+++ web-ui/public/js/script.js
@@ -1,34 +1,37 @@
+/*global firebase, document */
+/*jslint browser:true */
+"use strict";
+
 /**
  * Reads data from Firestore and updates information
  * displayed on the dashboard
  * @param {String} sensor The sensor key.
  */
-var readData = function(sensor) {
-  var db = firebase.firestore();
-  db.collection(sensor)
-    .onSnapshot(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-      document.getElementById(sensor).innerText = doc.data().value;
-      var today = new Date();
-      var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-      var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-      var dateTime = date+' '+time;
-      document.getElementById("last-update").innerText = dateTime;
-    });
-  });
+function readData(sensor) {
+    var db = firebase.firestore();
+    db.collection(sensor)
+        .onSnapshot(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                document.getElementById(sensor).innerText = doc.data().value;
+                var today = new Date();
+                var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                var dateTime = date + ' ' + time;
+                document.getElementById("last-update").innerText = dateTime;
+            });
+        });
 }
 
 /**
  * Triggered once DOM is loaded.
  */
-document.addEventListener('DOMContentLoaded', function() {
-  try {
-  	let app = firebase.app();
-  	let sensors = ["sensor1", "sensor2", "sensor3"];
-  	sensors.forEach(function(sensor) {
-  	  readData(sensor);
-  	});
-  } catch (e) {
-	  console.error(e);
-  }
+document.addEventListener('DOMContentLoaded', function () {
+    try {
+        var sensors = ["temperature", "humidity", "pressure"];
+        sensors.forEach(function (sensor) {
+            readData(sensor);
+        });
+    } catch (e) {
+        console.error(e);
+    }
 });
