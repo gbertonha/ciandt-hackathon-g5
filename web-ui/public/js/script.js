2ac7da1aae6c56f66ce6106e1db50a7047934f6c
ntezi <ngabomarius@gmail.com>
2019-11-09 13:45:15 +0900

    changing the screen colors

--- web-ui/public/js/script.js
+++ web-ui/public/js/script.js
@@ -12,7 +12,13 @@ function readData(sensor) {
     db.collection(sensor)
         .onSnapshot(function (querySnapshot) {
             querySnapshot.forEach(function (doc) {
-                document.getElementById(sensor).innerText = doc.data().value;
+
+                /* Get the temperature value */
+                var temperature = doc.data().value;
+                document.getElementById(sensor).innerText = temperature;
+                changeBackgroundColor(temperature)
+
+                // document.getElementById(sensor).innerText = doc.data().value;
                 var today = new Date();
                 var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
                 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
@@ -23,11 +29,31 @@ function readData(sensor) {
 }
 
 /**
+ * This function is for changing the screen colors
+ * when the temperature increases of decreases!
+ */
+function changeBackgroundColor(temperature) {
+    /* Change the screen colors */
+    var red_color = "#ff6666", blue_color = "#66a3ff", green_color = "#66ff66";
+    var agreed_temperature = 25
+
+    if (temperature > agreed_temperature) {
+        document.body.style.backgroundColor = red_color;
+    } else if (temperature < agreed_temperature) {
+        document.body.style.backgroundColor = blue_color;
+    } else {
+        document.body.style.backgroundColor = green_color;
+    }
+}
+
+/**
  * Triggered once DOM is loaded.
  */
 document.addEventListener('DOMContentLoaded', function () {
     try {
-        var sensors = ["temperature", "humidity", "pressure"];
+        var sensors = ["temperature"];
+
+        // var sensors = ["temperature", "humidity", "pressure"];
         sensors.forEach(function (sensor) {
             readData(sensor);
         });
